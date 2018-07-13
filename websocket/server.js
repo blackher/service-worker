3af780c3d374e0f9716ef5fdc81f56de89baaa09
shihe <ashitudou@126.com>
2018-07-13 19:18:29 +0800

    update

--- websocket/server.js
+++ websocket/server.js
@@ -23,17 +23,36 @@ function handler (req, res) {
 }
 
 io.on('connection', function (socket) {
-    //socket.emit('news', { hello: 'world' });
-    console.log(socket.id);
-    console.log(socket.rooms);
+
+    socket.on('disconnect', (reason) => {
+        // ...
+        console.log(reason);
+        socketPool.pop(this);//pop socket
+        console.log(socketPool.length);
+
+    });
+
+    socket.emit('news', { hello: 'world' });
+
     socket.join('room 123',()=>{
         let rooms = Object.keys(socket.rooms);
-        console.log(rooms); // [ <socket.id>, 'room 237' ]
+        //console.log(rooms); // [ <socket.id>, 'room 237' ]
+        socketPool.push(this);
+        console.log(socketPool.length);
+
     })
 
     //setTimeout("socket.emit('news',{hello:'world'})",1000)
-    socket.on('other', function (data) {
+    //socket.on('other', function (data) {
+        //console.log(data);
+    //});
+    socket.on('speak',function(data){
         console.log(data);
-    });
+        //其他人的聊天广播
+        socket.broadcast.emit('other speak',data);
+
+    })
+
 });
+
 
